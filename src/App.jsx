614876d0c987d7e22e72060551f7510d78143eaa
Abip-82 <abipmahat04@gmail.com>
2026-06-03 11:33:13 +0545

    rendering UI from arrays

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,26 @@ import './App.css'
 
 function App() {
 
-const [inputText, setInputText] =useState('') 
-const [todos , setTodos] = useState([])
+  const [inputText, setInputText] =useState('') 
+  const [todos , setTodos] = useState([])
+
+  function handleAddTodo(){
+    setTodos([...todos, inputText])
+    setInputText('')
+  }
 
   return(
     <>
       <div>To-do app</div>
       <input type="text" placeholder="Enter a task" value={inputText} onChange={(event) => setInputText(event.target.value)}/>
-      <button>Add Task</button>
-    </>
+      <button onClick={handleAddTodo}>Add Task</button>
+      <div>
+        <h2>To DO List:</h2>
+        <ul>
+          {todos.map((todo,index) => <li key={index}>{todo}</li>)}
+        </ul>
+      </div>
+    <>
 
   )
 }
