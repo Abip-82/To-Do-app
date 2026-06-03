ac3a7acfe9b3eef3adbd9e15bbe3265a6418c7af
Abip-82 <abipmahat04@gmail.com>
2026-06-03 12:45:23 +0545

    deleting functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,45 @@
-import {useState} from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  const [inputText, setInputText] =useState('') 
-  const [todos , setTodos] = useState([])
-
-  function handleAddTodo(){
-    setTodos([...todos, inputText])
-    setInputText('')
+  function handleAddTodo() {
+    setTodos([...todos, inputText]);
+    setInputText("");
+  }
+  function handleDeleteTodo(indexDelete) {
+    const newTodos = todos.filter((todo,index) => {
+      return (
+        index !== indexDelete
+      )
+    })
+    setTodos(newTodos)
   }
 
-  return(
+  return (
     <>
       <div>To-do app</div>
-      <input type="text" placeholder="Enter a task" value={inputText} onChange={(event) => setInputText(event.target.value)}/>
+      <input
+        type="text"
+        placeholder="Enter a task"
+        value={inputText}
+        onChange={(event) => setInputText(event.target.value)}
+      />
       <button onClick={handleAddTodo}>Add Task</button>
       <div>
         <h2>To DO List:</h2>
         <ul>
-          {todos.map((todo,index) => <li key={index}>{todo}</li>)}
+          {todos.map((todo, index) => (
+            <li key={index}>{todo}
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+            </li>
+          ))}
         </ul>
       </div>
-    <>
-
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
